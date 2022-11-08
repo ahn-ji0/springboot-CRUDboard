410d1167c28f706554141732a030dd810024d556
Jiyoung <ahn.jiyoung15@gmail.com>
2022-11-08 15:43:56 +0900

    -

---
+++ src/main/java/com/mustache/bbspractice/SpringbootMustachePracticeApplication.java
@@ -0,0 +1,13 @@
+package com.mustache.bbspractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMustachePracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMustachePracticeApplication.class, args);
+    }
+
+}
