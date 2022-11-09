318482f7f04f908d4e59b77ad272187f28138a97
Jiyoung <ahn.jiyoung15@gmail.com>
2022-11-09 14:27:26 +0900

    Add: variable id, Fix: Constructor, toEntity

--- src/main/java/com/mustache/bbspractice/domain/dto/ArticleDto.java
+++ src/main/java/com/mustache/bbspractice/domain/dto/ArticleDto.java
@@ -3,13 +3,22 @@ package com.mustache.bbspractice.domain.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-@AllArgsConstructor
 @Getter
 public class ArticleDto {
+    private Long id;
     private String title;
     private String content;
+
+    public ArticleDto(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
 
     public Article toEntity(){
         return new Article(title,content);
     }
+    public Article toEntity(Long id){
+        return new Article(id, title,content);
+    }
+
 }
